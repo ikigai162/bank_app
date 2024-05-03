4a49821aa6014e7c907a3136bff59cc559cc4b62
alexandru_plinschi <top@global.com>
2024-05-03 21:38:33 +0300

    Added clients file

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          className={`${styles.flexCenter} sm:min-w-[192px] min-w-[120px] flex-1 hover:`}
+          key={client.id}
+        >
+          <img
+            src={client.logo}
+            alt={client}
+            className="sm:w-[192px] w-[100px] object-contain hover:bg-white ease-in-out transition-all p-3 rounded-xl cursor-pointer"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-export default Clients
+export default Clients;
